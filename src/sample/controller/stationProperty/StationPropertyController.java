8f8be1191305a9ca4eea8dea6280ce894670c596
hossein sharifi <hosseinsharifi@hotmail.com>
2018-01-04 10:19:11 +0000

    start to modify station properties button for setting validator for inputs

--- src/sample/controller/stationProperty/StationPropertyController.java
+++ src/sample/controller/stationProperty/StationPropertyController.java
@@ -1,21 +1,15 @@
 package sample.controller.stationProperty;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class StationPropertyController {
-
+    @FXML
     public TextField nitrogenTextField = new TextField();
     @FXML
-    ComboBox gasPercentType = new ComboBox(FXCollections.observableArrayList(
-            "Option 1",
-            "Option 2",
-            "Option 3"
-    ));
+    ComboBox gasPercentType = new ComboBox();
     @FXML
     TextField textField = new TextField();
     @FXML
@@ -24,17 +18,18 @@ public class StationPropertyController {
         gasPercentType.getItems().addAll("درصد مولی", "درصد جرمی");
         gasPercentType.getSelectionModel().select("درصد مولی");
 
-        nitrogenTextField.focusedProperty().addListener((arg0, oldValue, newValue) -> {
-            if (!newValue) { //when focus lost
-                if(!nitrogenTextField.getText().matches("[1-5]\\.[0-9]|6\\.0")){
-                    //when it not matches the pattern (1.0 - 6.0)
-                    //set the textField empty
-                    nitrogenTextField.setText("");
-                    nitrogenTextField.clear();
-                }
-            }
-
-        });
+//        nitrogenTextField.focusedProperty().addListener((arg0, oldValue, newValue) -> {
+//            if (!newValue) { //when focus lost
+//                if(!nitrogenTextField.getText().matches("^(0|[1-9][0-9]*)$")){
+//                    //when it not matches the pattern (1.0 - 6.0)
+//                    //set the textField empty
+//                    System.out.println("inside if");
+//                    nitrogenTextField.setText("");
+//                    nitrogenTextField.clear();
+//                }
+//            }
+//
+//        });
     }
 
 
--- src/sample/view/stationProperty/StationPropertyFrame.java
+++ src/sample/view/stationProperty/StationPropertyFrame.java
@@ -1,23 +1,34 @@
 package sample.view.stationProperty;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sample.view.base.BaseFrame;
 
+import java.awt.*;
 import java.io.IOException;
 
-public class StationPropertyFrame implements BaseFrame {
+public class StationPropertyFrame extends Application implements BaseFrame {
 
     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("stationProperty.fxml"));
     Parent root1 = (Parent) fxmlLoader.load();
     Stage stage = new Stage();
+    @FXML
+    TextField nitrogenTextField = new TextField();
 
     public StationPropertyFrame() throws IOException {
         stage.setScene(new Scene(root1, 500, 1000));
         stage.setTitle("اطلاعات ایستگاه گاز");
+    }
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        show();
     }
 
     @Override
