43e65250b89a7e4c6cf61b41f36304ac3a3aa1ee
hossein sharifi <hosseinsharifi@hotmail.com>
2018-01-20 02:17:30 +0330

    add table to result show

--- src/sample/controller/showResults/ShowResultsController.java
+++ src/sample/controller/showResults/ShowResultsController.java
@@ -2,12 +2,26 @@ package sample.controller.showResults;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class ShowResultsController {
 
+    public TableView table;
     @FXML
     TextField textField = new TextField();
+    @FXML
+    public void initialize() throws IOException {
+        TableColumn firstNameCol = new TableColumn("نام");
+        TableColumn lastNameCol = new TableColumn("مقدار");
+
+        table.getColumns().addAll(firstNameCol, lastNameCol);
+
+
+    }
 
     public void okAction(ActionEvent actionEvent) {
         System.out.println(textField.getText());
